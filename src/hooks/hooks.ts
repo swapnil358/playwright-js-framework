d367ec257b9fa5014b11b9c7ba9a6079a03e232e
swapnil358 <swapnil.bodade88@gmail.com>
2024-02-24 15:51:52 +0530

    hooks added

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,17 +1,43 @@
-import { BeforeAll, AfterAll, Before, After } from "@cucumber/cucumber";
-import { chromium, Browser, Page } from "@playwright/test";
+import { BeforeAll, AfterAll, Before, After, BeforeStep, AfterStep, Status } from "@cucumber/cucumber";
+import { chromium, Browser, Page, BrowserContext } from "@playwright/test";
 import { pageFixture } from "./pageFixture";
 
-let page: Page;
+
 let browser: Browser;
+let context: BrowserContext;
+BeforeAll(async function () {
+  browser = await chromium.launch({ headless: false });
+})
 
 Before(async function () {
-  browser = await chromium.launch({ headless: false });
-  page = await browser.newPage();
+  context = await browser.newContext();
+  const page = await browser.newPage();
   pageFixture.page = page;
 });
 
-After(async function () {
+//if you want screenshot after each step
+// AfterStep(async function ({ pickle }) {
+//   const screesnhot = await pageFixture.page.screenshot({ path: `./test-result/screenshots/${pickle.name}.png`, type: "png" });
+//     await this.attach(screesnhot, "image/png");
+// })
+
+
+
+
+After(async function ({pickle, result}) {
+  console.log(result?.status);
+  
+  //screenshot
+  if (result?.status == Status.FAILED) {
+    const screesnhot = await pageFixture.page.screenshot({ path: `./test-result/screenshots/${pickle.name}.png`, type: "png" });
+    await this.attach(screesnhot, "image/png");
+  }
+
+
   await pageFixture.page.close();
-  await browser.close();
+  await context.close();
 });
+
+AfterAll(async function(){
+  await browser.close();
+})
