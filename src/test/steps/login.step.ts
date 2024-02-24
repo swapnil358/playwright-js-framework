8a6f2b2675ef7b79461258ebaf54479044f89948
swapnil358 <swapnil.bodade88@gmail.com>
2024-02-24 10:58:02 +0530

    first commit

---
+++ src/test/steps/login.step.ts
@@ -0,0 +1,64 @@
+import { Given, Then, When } from "@cucumber/cucumber";
+
+Given('User navigates to the application', async function () {
+    console.log("Hello world")
+});
+  
+
+
+  Given('User click on the login link', async function () {
+    return 'pending';
+  });
+
+
+  Given('User enter the username as {string}', async function (string) {
+    return 'pending';
+  });
+
+
+
+  Given('User enter the password as {string}', async function (string) {
+  
+    return 'pending';
+  });
+
+
+
+  When('User click on the login button', async function () {
+
+    return 'pending';
+  });
+
+  Then('Login should be success', async function () {
+    return 'pending';
+  });
+
+
+
+  Given('User click on the login link', async function () {
+    return 'pending';
+  });
+
+
+  Given('User enter the username as {string}', async function (string) {
+    return 'pending';
+  });
+
+
+
+  Given('User enter the password as {string}', async function (string) {
+    return 'pending';
+  });
+
+
+  When('User click on the login button', async function () {
+    return 'pending';
+  });
+
+
+
+  When('Login should fail', async function () {
+    return 'pending';
+  });
+
+
